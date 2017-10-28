c61ec1a44a0edc9d1f985ff57b03ee1d55c2acbc
X Zhang <zhangxd6@hotmail.com>
2017-10-27 21:18:48 -0500

    more icons files

--- App/Containers/Test.tsx
+++ App/Containers/Test.tsx
@@ -21,7 +21,7 @@ class TestScreen extends React.Component<{}, {}> {
                             < View style= { styles.section } >
                                 <Image source={ Images.ready } />
                                 < Text style={styles.sectionText} >
-                            this from test again!
+                            this from test for code puhs!
                                         This probably isn't what your app is going to look like. Unless your designer handed you this screen and, in that case, congrats! You're ready to ship.For everyone else, this is where you'll see a live preview of your fully functioning app using Ignite.
             </Text>
             </View>
