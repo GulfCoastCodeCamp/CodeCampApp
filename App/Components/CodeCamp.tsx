ea6f1c176e205b034422f760c39ae0d6cb9e19e9
X Zhang <zhangxd6@hotmail.com>
2017-11-05 10:40:06 -0600

    add twitter and facebook button

--- App/Components/CodeCamp.tsx
+++ App/Components/CodeCamp.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
-import { View, Text, Image, Linking } from 'react-native'
+import { View, Text, Image, Linking, TouchableOpacity} from 'react-native'
 // import LinearGradient from 'react-native-linear-gradient'
 import RoundedButton from './RoundedButton'
 import { Images } from '../Themes'
 import styles from './Styles/CodeCampStyles'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
+const twitterus= () => {
+  const appURL = 'twitter://post?screen_name=gccodecamp'
+  const webURL = 'https://twitter.com/intent/user?screen_name=gccodecamp'
+  Linking.canOpenURL(appURL).then((supported) =>
+    Linking.openURL(supported ? appURL : webURL)
+  )
+}
+
+const facebookus = () => {
+  const appURL = 'facebook://gccodecamp'
+  const webURL = 'https://www.facebook.com/gccodecamp/'
+  Linking.canOpenURL(appURL).then((supported) =>
+    Linking.openURL(supported ? appURL : webURL)
+  )
+}
 const CodeCamp = (props) => {
   const gradient = ['#351F41', '#8E2044']
 
@@ -15,6 +31,12 @@ const CodeCamp = (props) => {
       <Image style={styles.infiniteRedIcon} source={Images.codeCamp} />
       <View style={styles.line} />
       <Text style={styles.subheading}>Keep the conversation going by{'\n'}connecting with Gulf Coast Code Camp</Text>
+      <TouchableOpacity onPress={() => twitterus()}>
+        <Icon name="twitter" size={35} />  
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => facebookus()}>
+        <Icon name="facebook" size={35} />
+      </TouchableOpacity>
     </View>
   )
 }
--- Features/schedules.ts
+++ Features/schedules.ts
@@ -4,26 +4,62 @@ const data = {
         "type": "break",
         "title": "Registration and Breakfast",
         "trackNumber": 0,
-        "time": "7/10/2017 8:45 AM",
+        "time": "9/30/2017 8:30 AM",
         "duration": "30",
     },
     {   "id":1,
         "type": "talk",
-        "speaker": "Jamon Holmgren",
-        "image": "jamon",
-        "title": "Welcome to Chain React",
-        "description": "Welcome to our Conference!  Greetings opening remarks",
-        "time": "7/10/2017 8:45 AM",
-        "duration": "5",
+        "speaker": "Brandon Atkins",
+        "image": "brandona",
+        "title": "TypeScript at Scale",
+        "description": "Discussing how to make enormous JavaScript codebases more scalable and maintainable across teams. As well as demoing many of the key features in TypeScript and how to get started.",
+        "time": "9/30/2017 9:00 AM",
+        "duration": "60",
         "track": "Web/Mobile",
         "trackNumber": 0,
         "speakerInfo": [
             {
-                "name": "Jamon Holmgren",
-                "bio": "Jamon Holmgren is the COO and one of the founders for Infinite Red.",
-                "twitter": "jamonholmgren",
-                "github": "jamonholmgren",
-                "company": "Infinite Red"
+                "name": "Brandon Atkins",
+                "bio": "Brandon is a full stack software developer who enjoys building interactive web applications and has experience in building single page applications.",
+                "twitter": "",
+                "github": "",
+                "company": ""
+            }
+        ]
+        },
+    {
+        "id": 2,
+        "type": "break",
+        "title": "Break",
+        "trackNumber": 0,
+        "time": "9/30/2017 10:00 AM",
+        "duration": "30",
+    },
+    {
+        "id": 3,
+        "type": "talk",
+        "speaker": "Billy `Obie` Hardin",
+        "image": "Billy",
+        "title": "How I Hacked your Mother: A developers peak inside the hacker's playbook",
+        "description": `
+When building a cloud app, one of the most common cross cutting concerns is the authentication of users. For some solutions, the security implementation is handled by the application itself. In this session, we explore the use of Azure Active Directory(AAD) to provide the security services for our web or mobile apps.The application we are building is a Node.js backend API with an AngularJS front end.
+
+We explore the following areas:
+        - Create an AngularJS App and use Git to deploy to Azure
+        - Add Azure Active Directory(AD) service authentication to protect our app
+        - Discuss the limitation using simple AD authentication when enabling anonymous access to some routes
+- Enhance our security by taking direct control of the Azure AD APIs in our code to consume the Jason Web Token(JWT) from the Azure Identity provider`,
+        "time": "9/30/2017 10:30 AM",
+        "duration": "60",
+        "track": "Web/Mobile",
+        "trackNumber": 0,
+        "speakerInfo": [
+            {
+                "name": "Brandon Atkins",
+                "bio": "Brandon is a full stack software developer who enjoys building interactive web applications and has experience in building single page applications.",
+                "twitter": "",
+                "github": "",
+                "company": ""
             }
         ]
     },
